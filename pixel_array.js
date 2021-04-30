6683a63caa549652664b2bad7d3316c0c58199d1
TheCzechBagel <46883875+TheCzechBagel@users.noreply.github.com>
2021-04-30 22:15:04 +1200

    Pixel Art Page completed

---
+++ pixel_array.js
@@ -0,0 +1,8 @@
+var pixel_array = [
+    {"name": "Tanjiro Kamado", "id": "tanjiro"},
+    { "name": "Kokushibo", "id": "kokushibo"},
+    { "name": "Zenitsu Agatsuma", "id": "zenitsu" },
+    { "name": "Giyuu Tomioka", "id": "giyuu"},
+    { "name": "Akaza", "id": "akaza"},
+    { "name": "Phoenix Wright", "id": "wright"},
+]
