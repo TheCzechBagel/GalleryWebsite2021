afb0ba542d208c5fa40abefc32cec1eea6b4d96c
TheCzechBagel <46883875+TheCzechBagel@users.noreply.github.com>
2021-05-05 21:22:42 +1200

    Updated Paint Drawing page
    Large design overhaul

--- paint_array.js
+++ paint_array.js
@@ -1,7 +1,6 @@
 var paint_array = [
-    {"name": "", "id": "", "descrip": ""},
-    {"name": "", "id": "", "descrip": ""},
-    {"name": "", "id": "", "descrip": ""},
-    {"name": "", "id": "", "descrip": ""},
-    {"name": "", "id": "", "descrip": ""},
+    {"bgid": "bg1","name": "A little light exercise", "id": "Post1", "descrip": "Does your body temperature reach critically low levels when you exercise? Well apparently one of my friend's does. It's ok, one of us will always be nearby with a flamethrower in case an emergency rescue is in order."},
+    {"bgid": "bg2","name": "The correct way to approach a sleeping dog", "id": "Post2", "descrip": "Everyone has heard the phrase 'let sleeping dogs lie', but how do you follow this law of life when said dog is taking up your position on the bed? The easiest and most obvious solution is an orbital drop with a parachute. By landing with your limbs around the dog [careful not to land directly on top of it] you can enjoy a peaceful night's rest while still following the greatest rule of modern society."},
+    {"bgid": "bg3","name": "Eating Soil", "id": "Post3", "descrip": "If I'm being perfectly honest I don't really know what's going on here either."},
+    {"bgid": "bg4","name": "A Good Kick", "id": "Post4", "descrip": "It's important that everyone has a good kick every now and them to remind themselves of where they are. This compilation of kickers is part of a running joke within our group."},
 ]
